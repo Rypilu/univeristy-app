12d3f2dfbc0de8cbe0b889de6226aa1d5283fcf6
rypilu <rpiluski@gmail.com>
2022-07-06 07:24:10 -0400

    student show page and edit student functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,10 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
     @student = Student.new
   end
@@ -17,6 +21,22 @@ class StudentsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+
 
   private
 
