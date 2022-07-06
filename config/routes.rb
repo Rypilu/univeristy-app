c7124b5c89d7f7327e2036d5bfca50d083e1eb7a
rypilu <rpiluski@gmail.com>
2022-07-06 06:29:19 -0400

    student table, routes, student index page and student sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,14 @@ Rails.application.routes.draw do
   get 'courses/new', to: 'courses#new'
   # go to /about and send that to pages controller which will point to about view
   get 'about', to: 'pages#about'
+  #  ~~~  using resources give you all routes  ~~~
+  #  students GET   /students(.:format)               students#index
+  #  POST  /students(.:format)                        students#create
+  #  new_student GET   /students/new(.:format)        students#new
+  #  edit_student GET   /students/:id/edit(.:format)  students#edit
+  #  student GET   /students/:id(.:format)            students#show
+  #  PATCH /students/:id(.:format)                    students#update
+  #  PUT   /students/:id(.:format)                    students#update
+  resources :students, except: [:destroy]
 
 end
