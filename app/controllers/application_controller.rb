01d88aa31fe555600bbba54165269624b9ddb4fa
rypilu <rpiluski@gmail.com>
2022-07-08 05:35:13 -0400

    create and destroy sessions, add auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # "||=" or "or-equals" is being used for Memoization
+    # This uses a logical OR (||) between left and right values, then assigns the result to the variable on the left.
+    # With this design, we will only perform the expensive DB query if @current_user doesn't exist - allowing us to skip a db lookup.
+    # https://www.honeybadger.io/blog/ruby-rails-memoization/
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    # !! turns this into a bool
+    !!current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-  # courses controler # index action
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
-  # go to /about and send that to pages controller which will point to about view
   get 'about', to: 'pages#about'
-  #  ~~~  using resources gives you all routes  ~~~
-  #  students GET   /students(.:format)               students#index
-  #  POST  /students(.:format)                        students#create
-  #  new_student GET   /students/new(.:format)        students#new
-  #  edit_student GET   /students/:id/edit(.:format)  students#edit
-  #  student GET   /students/:id(.:format)            students#show
-  #  PATCH /students/:id(.:format)                    students#update
-  #  PUT   /students/:id(.:format)                    students#update
   resources :students, except: [:destroy]
-
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
